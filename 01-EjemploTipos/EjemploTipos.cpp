ff60d1746920b5ec171e25d368bb8bc25cc5c6d7
eonishi <104165553+eonishi@users.noreply.github.com>
2022-06-02 14:44:02 -0300

    Update EjemploTipos.cpp

--- 01-EjemploTipos/EjemploTipos.cpp
+++ 01-EjemploTipos/EjemploTipos.cpp
@@ -1,24 +1,33 @@
-#include<cassert>
+#include <cassert>
+#include <string>
+
+using namespace std::string_literals;
 
 int main(){
 
-    //bool
+    //bool 
     assert (true != false);
+    assert(not false); 
 
     //unisgned
-    assert (1+2 == 3);
-    assert (6%5 == 1);
+    assert(56u + 57u == 113u);
 
     //char
     assert ('h' <= 'x');
     assert ('h' <= 'h');
+    assert('C' + 2 == 'E');
+    assert('C' - 2 == 65);
 
     //int
     assert (-506+(-988) >= -8*650);
 
     //double
     assert (240.7932147/340.872871945 > 0.66565776453);
+    assert(0.03 == 3e-2);
 
     //strings
-    assert ("Carlos180" < "Carlos20");
+    assert ("Carlos180"s < "Carlos20"s);
+    assert(7 == "bom Dia"s.length());
+    assert(14 < "bom dia parceiro"s.length());
+    assert("skill"s == "s"s + "kill"s);
 }
